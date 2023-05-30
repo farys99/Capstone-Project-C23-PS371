34b7305076038be16e430cf59e97796a345c0f8b
farys99 <stackb811@gmail.com>
2023-05-30 16:04:34 +0700

    fill main activity

--- app/src/main/java/com/example/dermadetect/MainActivity.kt
+++ app/src/main/java/com/example/dermadetect/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.example.dermadetect
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.dermadetect.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.skinProblemsDetection.setOnClickListener{
+            skinDetectionProblem(SKIN_PROBLEM)
+
+            val intent = Intent(this, PostingActivity::class.java)
+            startActivity(intent)
+        }
+
+
+
     }
+    private fun skinDetectionProblem(skin : String){
+        val setting = getSharedPreferences(MENU_SKIN_PROBLEM, Context.MODE_PRIVATE)
+        val edit = setting.edit()
+        edit.putString(KEY_SKIN_PROBLEM, skin)
+        edit.apply()
+    }
+
+    companion object{
+        const val MENU_SKIN_PROBLEM = "menu_skin_problem"
+        const val SKIN_PROBLEM = "skin_problem"
+        const val KEY_SKIN_PROBLEM = "key_skin_problem"
+    }
+
 }
