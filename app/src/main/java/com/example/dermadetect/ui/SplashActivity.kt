a989dd4465621d416b77ef99629caa416a23bd44
farys99 <stackb811@gmail.com>
2023-06-10 02:33:59 +0700

    splashscreen

---
+++ app/src/main/java/com/example/dermadetect/ui/SplashActivity.kt
@@ -0,0 +1,21 @@
+package com.example.dermadetect.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import androidx.core.content.ContextCompat.startActivity
+import com.example.dermadetect.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 2000)
+    }
+}
